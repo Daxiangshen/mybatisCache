9e8642d9f7de399ab4b7adc4756dba35d29c91d8
yuxiang123 <yuxiang@innjoy.me>
2019-07-11 17:32:39 +0800

    逻辑代码编写

--- src/main/java/com/mybatiscache/config/Swagger2Config.java
+++ src/main/java/com/mybatiscache/config/Swagger2Config.java
@@ -1,17 +1,13 @@
 package com.mybatiscache.config;
 
-import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Predicate;
+
 
 /***
  * Swagger2Config class
